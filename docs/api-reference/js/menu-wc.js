612e0f6542573157e874b377ca580d7a850a58e2
GeoAstronaute <jeanmarc.royy@gmail.com>
2019-09-10 11:32:16 +0200

    (Docs) Update demo + api-reference

--- docs/api-reference/js/menu-wc.js
+++ docs/api-reference/js/menu-wc.js
@@ -35,6 +35,11 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                             </a>
                         </li>
                         <li class="link">
+                            <a href="license.html"  data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>LICENSE
+                            </a>
+                        </li>
+                        <li class="link">
                             <a href="dependencies.html" data-type="chapter-link">
                                 <span class="icon ion-ios-list"></span>Dependencies
                             </a>
