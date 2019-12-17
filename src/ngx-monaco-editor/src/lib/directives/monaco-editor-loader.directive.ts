3f0270deac48b10664497aaafe5489965d3e8e02
GeoAstronaute <jeanmarc.royy@gmail.com>
2019-12-17 10:28:17 +0100

    Refac: unsubscribe on destroy + remove input

--- src/ngx-monaco-editor/src/lib/directives/monaco-editor-loader.directive.ts
+++ src/ngx-monaco-editor/src/lib/directives/monaco-editor-loader.directive.ts
@@ -1,22 +1,30 @@
-import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, TemplateRef, ViewContainerRef, OnDestroy, OnInit } from '@angular/core';
 import { MonacoEditorLoaderService } from '../services/monaco-editor-loader.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
-@Directive({ selector: '[materiaLoadMonacoEditor]' })
-export class MonacoEditorLoaderDirective {
-    @Input() set loadMonacoEditor(value: any) {
-        this.monacoEditorLoaderService.monacoPath = value;
-    }
+@Directive({ selector: '[ngxLoadMonacoEditor]' })
+export class MonacoEditorLoaderDirective implements OnInit, OnDestroy {
+  isMonacoLoaded$ = this.monacoEditorLoaderService.isMonacoLoaded.asObservable();
+  destroyed$ = new Subject<void>();
 
-    constructor(
-        private templateRef: TemplateRef<any>,
-        private viewContainer: ViewContainerRef,
-        private monacoEditorLoaderService: MonacoEditorLoaderService) {
-        monacoEditorLoaderService.isMonacoLoaded.subscribe(isLoaded => {
-            if (isLoaded) {
-                this.viewContainer.createEmbeddedView(this.templateRef);
-            } else {
-                this.viewContainer.clear();
-            }
-        });
-    }
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef,
+    private monacoEditorLoaderService: MonacoEditorLoaderService
+  ) {}
+
+  ngOnInit() {
+    this.isMonacoLoaded$.pipe(takeUntil(this.destroyed$)).subscribe((loaded) => {
+      if (! loaded) {
+        return this.viewContainer.clear();
+      }
+      this.viewContainer.createEmbeddedView(this.templateRef);
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
 }
